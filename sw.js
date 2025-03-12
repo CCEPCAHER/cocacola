74c7dcf25230d6e96fe532af39d67d59218313b3
CCEPCAHER <franklin_carranza@hotmail.com>
2025-03-12 11:54:18 +0100

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,22 @@
-const CACHE_NAME = 'mi-app-cache-v1';
-const urlsToCache = [
-  '/cocacola/',
-  '/cocacola/index.html',
-  '/cocacola/style.css',
-  '/cocacola/manifest.json',
-  '/cocacola/icons/icon-192.png',
-  '/cocacola/icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Cargando en caché los recursos');
-      return cache.addAll(urlsToCache);
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/manifest.json",
+        "/icons/icon-192x192.png",
+        "/icons/icon-512x512.png"
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
