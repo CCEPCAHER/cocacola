8e7d70a5359ad3990d6d941f91e5746e097f2732
CCEPCAHER <franklin_carranza@hotmail.com>
2025-04-05 19:26:59 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -207,6 +207,42 @@ document.addEventListener("DOMContentLoaded", function() {
   "staticOffer": true,
 },
 { 
+  "name": "", 
+  "price": 0.00, 
+  "offer": false,
+  "staticOffer": true,
+},
+{ 
+  "name": "", 
+  "price": 0.00, 
+  "offer": false,
+  "staticOffer": true,
+},
+{ 
+  "name": "", 
+  "price": 0.00, 
+  "offer": false,
+  "staticOffer": true,
+},
+{ 
+  "name": "", 
+  "price": 0.00, 
+  "offer": false,
+  "staticOffer": true,
+},
+{ 
+  "name": "", 
+  "price": 0.00, 
+  "offer": false,
+  "staticOffer": true,
+},
+{ 
+  "name": "", 
+  "price": 0.00, 
+  "offer": false,
+  "staticOffer": true,
+},
+{ 
   "name": "",  
   "price": 0.00,
   "offer": false,
