3ff2b5570805540eea90290d84560912616c2225
CCEPCAHER <ccipbcncrd4@grupocaher.com>
2026-04-24 07:05:49 +0200

    Fix: sw.js relative paths for GitHub Pages and v7

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = "cocacola-fem-v6";
-const DYNAMIC_CACHE = "cocacola-dynamic-v6";
-const IMAGE_CACHE = "cocacola-images-v6";
+const CACHE_NAME = "cocacola-fem-v7";
+const DYNAMIC_CACHE = "cocacola-dynamic-v7";
+const IMAGE_CACHE = "cocacola-images-v7";
 
 const ASSETS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/ui.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./ui.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 function getCleanUrl(url) {
